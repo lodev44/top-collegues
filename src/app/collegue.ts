a87156c36acd3f28842c8268bf39b19e55c58fbf
Laurence Ott <lodev.diginamic@gmail.com>
2022-02-09 14:33:40 +0100

    Mise en place de la structure des composants collegues et avis

---
+++ src/app/collegue.ts
@@ -0,0 +1,5 @@
+export interface Collegue {
+  pseudo: string;
+  score: number;
+  photoUrl: string;
+}
---
+++ src/app/composants/collegue/collegue.component.ts
@@ -0,0 +1,16 @@
+import { Collegue } from './../../collegue';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-collegue',
+  templateUrl: './collegue.component.html',
+  styleUrls: ['./collegue.component.scss']
+})
+export class CollegueComponent implements OnInit {
+
+  constructor() { }
+//@Input() collegue:Collegue;
+  ngOnInit(): void {
+  }
+
+}
